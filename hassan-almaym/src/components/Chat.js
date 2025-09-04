0ca6637d96e7e9cad94369d3334c34d2fb0ee600
Hassanalmaymn <144214579+Hassanalmaymn@users.noreply.github.com>
2025-09-04 11:47:12 +0300

    postitin of chat changed

--- hassan-almaym/src/components/Chat.js
+++ hassan-almaym/src/components/Chat.js
@@ -68,7 +68,7 @@ const Chat = ({ ui }) => {
   };
 
   return (
-    <div className="fixed bottom-4 left-4 z-50">
+    <div className="fixed bottom-4 right-4 z-50">
       {/* Chat button */}
       {!isOpen && (
         <button
